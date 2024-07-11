54db00950e13181de0540c7642d9ade3f247387f
malsha11 <96194@fhss.sjp.ac.lk>
2024-07-11 11:52:39 +0530

    Modified

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,7 @@
-//index.js - Serves as the entry point for your React application
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // Your main application component
+import App from './App'; 
 
 ReactDOM.render(
   <React.StrictMode>
